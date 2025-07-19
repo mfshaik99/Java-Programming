2027f4abf70eb49b1f8eff4d5155a5b2413137be
MUHAMMAD FARHAN SHAIK <24B81A66E3@cvr.ac.in>
2025-07-19 15:01:38 +0530

    Hello.java

---
+++ basics/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {  // This is a java class declaration.Java should have atleast one class
+  
+  public static void main(String[] args) { //This is a main method.The entry point of java program
+    
+    System.out.println("Hello to all developers! This is my Java Repository"); //This is the syntax to print any line to the output console.
+    
+  }
+  
+}
