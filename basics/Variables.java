313fe39fa9119d781398e51cfc235dae4d65f150
MUHAMMAD FARHAN SHAIK <24B81A66E3@cvr.ac.in>
2025-07-19 15:24:10 +0530

    Variables.java

---
+++ basics/Variables.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+    // This is a way to declare a int variable in java (same like C)
+    int age = 18;
+    // This is the way to declare a string variable 
+    String name = "Farhan";
+    // This is the way to declare a Boolean variable 
+    boolean isStudent = true;
+    //String should be Mention in (" ")
+    String hobby = "Coding";
+    System.out.println("Name: " + name); // The + opearation is used to Concatenate the Variables
+    System.out.println("Age: " + age);
+    System.out.println("Is a student: " + isStudent);
+    System.out.println("Hello! My name is " + name + ". I am " + age + " years old, and I love " + hobby + ".");
+  }
+}
