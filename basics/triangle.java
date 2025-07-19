6f8f94d30423fa252fb8ccfa4a57848bc9d55e64
MUHAMMAD FARHAN SHAIK <24B81A66E3@cvr.ac.in>
2025-07-19 15:15:54 +0530

    triangle.java

---
+++ basics/triangle.java
@@ -0,0 +1,11 @@
+// In this program we are going to create a triangular shape by using a print statements (i know it is Basic but it is imp to understand how "println" works !)
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("     /|");
+    System.out.println("    / |");
+    System.out.println("   /  |");
+    System.out.println("  /   |");
+    System.out.println(" /____|");
+  }
+}
